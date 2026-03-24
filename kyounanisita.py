8221f5bc883c5ed7e24b1bcc2a8be8dcd027360a
as <megumiru175@gmail.com>
2026-03-24 14:19:48 +0900

    WIP

--- kyounanisita.py
+++ kyounanisita.py
@@ -1,19 +1,33 @@
 import streamlit as st # type: ignore
+
 from random import choice
-st.title('今日はこれができたよ！！')
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #ffff00;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.markdown("<h2 style = 'color:blue;'>今日はこれができたよ！！'</h2>",
+            unsafe_allow_html=True
+)
+   
 st.subheader('やらなくてはいけないことたくさんあるよね\n'
         'それをひとつずつやってしまうおてつだいをするよ')
 
 #name
 name = st.text_input('名前をいれてね٩꒰｡•◡•｡꒱۶')
 
-#もし多かったらラジオボタンかこれ以上でほめる。
+#もし多かったらラジオボタンかこれ以上でほめる。git init
 #ボタン
 submit_btn = st.button('これでいいかな？')
 cancel_btn = st.button('やっぱりやめておく')
 if submit_btn:
     st.text(f'ようこそ、{name}さん！')
-
 st.write(f"今日は{name}さんは何をしたのかな？")
 
 #複数選択
@@ -28,6 +42,8 @@ st.write(f"すごいな{name}さん！！(ᗒ⩊ᗕ)⸝ި ʕᦏ")
 st.write ( "そんなすごい人には、プレゼントがあるよ🎁")
 
 if st.button("何がでるかな？？"):
-    r = choice(["happy","大吉","おめでとう！"])
+    r = choice(["happy","大吉","おめでとう！",""])
     st.write(r)
-    st.write ( "あしたもがんばろうね☆")
+    if st.button("おしてみてね"):
+        st.balloons() 
+    st.write ( "あしたもがんばろうね(๑•̀ㅂ•́)و✧")
