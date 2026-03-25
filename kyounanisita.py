0604c58124b39cd6268c4f9fa41c31f8fd3b42cc
as <megumiru175@gmail.com>
2026-03-25 11:12:50 +0900

    Update kyounanisita.py

--- kyounanisita.py
+++ kyounanisita.py
@@ -12,7 +12,6 @@ st.markdown(
 )
 st.markdown("<h2 style = 'color:blue;'>これができたよ！！</h2>",
             unsafe_allow_html=True)
-url("https://drive.google.com/file/d/1dt46pDdpV5K5h2nnzoNEgoocEf-bLlRN/view?usp=sharing")
 st.subheader('やらなくてはいけないことたくさんあるよね\n'
         'それをひとつずつやってしまうおてつだいをするよ')
 
